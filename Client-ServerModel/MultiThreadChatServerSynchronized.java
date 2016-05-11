b909d6763e1c9182ec7185547e6a8293518bd052
Jay-611 <jaber-611@hotmail.com>
2016-05-11 10:52:00 -0700

    Update MultiThreadChatServerSynchronized.java

--- Client-ServerModel/MultiThreadChatServerSynchronized.java
+++ Client-ServerModel/MultiThreadChatServerSynchronized.java
@@ -1,246 +1,166 @@
 import java.io.DataInputStream;
-import java.io.PrintStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-/*
- * A chat server that delivers public and private messages.
- */
 public class MultiThreadChatServerSynchronized 
 {
-  // The server socket.
-  private static ServerSocket serverSocket = null;
-  // The client socket.
-  private static Socket clientSocket = null;
+    private static ServerSocket server = null;
+    private static Socket client = null;
 
-  // This chat server can accept up to maxClientsCount clients' connections.
-  private static final int maxClientsCount = 10;
-  private static final clientThread[] threads = new clientThread[maxClientsCount];
+    private static final int maximumClients = 10;
+    private static final clientThread[] clients = new clientThread[maximumClients];
 
-  public static void main(String args[]) 
-  {
-    // The default port number.
-    int portNumber = 2222;
-    if (args.length < 1) 
+    public static void main(String args[]) 
     {
-      System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
-          + "Now using port number=" + portNumber);
-    } 
-    else 
-    {
-      portNumber = Integer.valueOf(args[0]).intValue();
-    }
+        int port = 2222;
+        if (args.length < 1)
+            System.out.println("App is using the following port=" + port);
+        else
+            port = Integer.valueOf(args[0]).intValue();
 
-    /*
-     * Open a server socket on the portNumber (default 2222). Note that we can
-     * not choose a port less than 1023 if we are not privileged users (root).
-     */
-    try 
-    {
-      serverSocket = new ServerSocket(portNumber);
-    } 
-    catch (IOException e) 
-    {
-      System.out.println(e);
-    }
-
-    /*
-     * Create a client socket for each connection and pass it to a new client
-     * thread.
-     */
-    while (true) 
-    {
-      try 
-      {
-        clientSocket = serverSocket.accept();
-        int i = 0;
-        for (i = 0; i < maxClientsCount; i++) 
+        try 
         {
-          if (threads[i] == null) 
-          {
-            (threads[i] = new clientThread(clientSocket, threads)).start();
-            break;
-          }
-        }
-        if (i == maxClientsCount) 
+            server = new ServerSocket(port);
+        } 
+        catch (IOException e) 
         {
-          PrintStream os = new PrintStream(clientSocket.getOutputStream());
-          os.println("Server too busy. Try later.");
-          os.close();
-          clientSocket.close();
+            System.out.println(e);
         }
-      } 
-      catch (IOException e) 
-      {
-        System.out.println(e);
-      }
+
+        while (true) 
+        {
+            try 
+            {
+                client = server.accept();
+                int i = 0;
+                for (i = 0; i < maximumClients; i++) {
+                    if (clients[i] == null) {
+                        (clients[i] = new clientThread(client, clients)).start();
+                        break;
+                    }
+                }
+                if (i == maximumClients) {
+                    PrintStream os = new PrintStream(client.getOutputStream());
+                    os.println("Chat room is full. Try again later.");
+                    os.close();
+                    client.close();
+                }
+            } 
+            catch (IOException e) 
+            {
+                System.out.println(e);
+            }
+        }
     }
-  }
 }
 
-/*
- * The chat client thread. This client thread opens the input and the output
- * streams for a particular client, ask the client's name, informs all the
- * clients connected to the server about the fact that a new client has joined
- * the chat room, and as long as it receive data, echos that data back to all
- * other clients. The thread broadcast the incoming messages to all clients and
- * routes the private message to the particular client. When a client leaves the
- * chat room this thread informs also all the clients about that and terminates.
- */
-class clientThread extends Thread 
-{
-  private String clientName = null;
-  private DataInputStream is = null;
-  private PrintStream os = null;
-  private Socket clientSocket = null;
-  private final clientThread[] threads;
-  private int maxClientsCount;
+class clientThread extends Thread {
 
-  public clientThread(Socket clientSocket, clientThread[] threads) 
-  {
-    this.clientSocket = clientSocket;
-    this.threads = threads;
-    maxClientsCount = threads.length;
-  }
+    private String Name = null;
+    private DataInputStream dis = null;
+    private PrintStream ps = null;
+    private Socket client = null;
+    private final clientThread[] clients;
+    private int maximumClients;
 
-  public void run() 
-  {
-    int maxClientsCount = this.maxClientsCount;
-    clientThread[] threads = this.threads;
-
-    try 
+    public clientThread(Socket socket, clientThread[] clients) 
     {
-      /*
-       * Create input and output streams for this client.
-       */
-      is = new DataInputStream(clientSocket.getInputStream());
-      os = new PrintStream(clientSocket.getOutputStream());
-      String name;
-      while (true) 
-      {
-        os.println("Enter your name.");
-        name = is.readLine().trim();
-        if (name.indexOf('@') == -1) 
-        {
-          break;
-        } 
-        else 
-        {
-          os.println("The name should not contain '@' character.");
-        }
-      }
+        this.client = socket;
+        this.clients = clients;
+        maximumClients = clients.length;
+    }
 
-      /* Welcome the new the client. */
-      os.println("Welcome " + name
-          + " to our chat room.\nTo leave enter /quit in a new line.");
-      synchronized (this) 
-      {
-        for (int i = 0; i < maxClientsCount; i++) 
+    public void run() 
+    {
+        int maximumClients = this.maximumClients;
+        clientThread[] clients = this.clients;
+
+        try 
         {
-          if (threads[i] != null && threads[i] == this) 
-          {
-            clientName = "@" + name;
-            break;
-          }
-        }
-        for (int i = 0; i < maxClientsCount; i++) 
-        {
-          if (threads[i] != null && threads[i] != this) 
-          {
-            threads[i].os.println("*** A new user " + name
-                + " entered the chat room !!! ***");
-          }
-        }
-      }
-      /* Start the conversation. */
-      while (true) 
-      {
-        String line = is.readLine();
-        if (line.startsWith("/quit")) 
-        {
-          break;
-        }
-        /* If the message is private send it to the given client. */
-        if (line.startsWith("@")) 
-        {
-          String[] words = line.split("\\s", 2);
-          if (words.length > 1 && words[1] != null) 
-          {
-            words[1] = words[1].trim();
-            if (!words[1].isEmpty()) 
+            dis = new DataInputStream(client.getInputStream());
+            ps = new PrintStream(client.getOutputStream());
+            String name;
+            while (true) 
             {
-              synchronized (this) 
-              {
-                for (int i = 0; i < maxClientsCount; i++) 
-                {
-                  if (threads[i] != null && threads[i] != this
-                      && threads[i].clientName != null
-                      && threads[i].clientName.equals(words[0])) 
-                  {
-                    threads[i].os.println("<" + name + "> " + words[1]);
-                    /*
-                     * Echo this message to let the client know the private
-                     * message was sent.
-                     */
-                    this.os.println(">" + name + "> " + words[1]);
+                ps.println("Register your name.");
+                name = dis.readLine().trim();
+                if (!name.contains("@"))
                     break;
-                  }
-                }
-              }
+                else
+                    ps.println("'@' character should not be in the name.");
             }
-          }
-        } 
-        else 
-        {
-          /* The message is public, broadcast it to all other clients. */
-          synchronized (this) 
-          {
-            for (int i = 0; i < maxClientsCount; i++) 
-            {
-              if (threads[i] != null && threads[i].clientName != null) 
-              {
-                threads[i].os.println("<" + name + "> " + line);
-              }
-            }
-          }
-        }
-      }
-      synchronized (this) 
-      {
-        for (int i = 0; i < maxClientsCount; i++) 
-        {
-          if (threads[i] != null && threads[i] != this
-              && threads[i].clientName != null) 
-          {
-            threads[i].os.println("*** The user " + name
-                + " is leaving the chat room !!! ***");
-          }
-        }
-      }
-      os.println("*** Bye " + name + " ***");
 
-      /*
-       * Clean up. Set the current thread variable to null so that a new client
-       * could be accepted by the server.
-       */
-      synchronized (this) 
-      {
-        for (int i = 0; i < maxClientsCount; i++) 
-        {
-          if (threads[i] == this) 
-          {
-            threads[i] = null;
-          }
-        }
-      }
-      /*
-       * Close the output stream, close the input stream, close the socket.
-       */
-      is.close();
-      os.close();
-      clientSocket.close();
-    } 
-    catch (IOException e) {}
-  }
+            ps.println("Welcome aboard " + name + "\nTo exit this chat room type \"/quit\"");
+            synchronized (this) 
+            {
+                for (int i = 0; i < maximumClients  ; i++)
+                    if (clients[i] != null && clients[i] == this) 
+                    {
+                        Name = "@" + name;
+                        break;
+                    }
+                for (int i = 0; i < maximumClients  ; i++)
+                    if (clients[i] != null && clients[i] != this)
+                        clients[i].ps.println("*** " + name + " has joined the chat room ***");
+            }
+            while (true) 
+            {
+                String message = dis.readLine();
+                if (message.startsWith("/quit"))
+                    break;
+                if (message.startsWith("@")) 
+                {
+                    String[] splittedMessage = message.split("\\s", 2);
+                    if (splittedMessage.length > 1 && splittedMessage[1] != null) 
+                    {
+                        splittedMessage[1] = splittedMessage[1].trim();
+                        if (!splittedMessage[1].isEmpty()) 
+                        {
+                            synchronized (this) 
+                            {
+                                for (int i = 0; i < maximumClients; i++) 
+                                {
+                                    if (clients[i] != null && clients[i] != this && clients[i].Name != null && clients[i].Name.equals(splittedMessage[0])) 
+                                    {
+                                        clients[i].ps.println("<" + name + "> " + splittedMessage[1]);
+                                        this.ps.println(">" + name + "> " + splittedMessage[1]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+                else 
+                {
+                    synchronized (this) 
+                    {
+                        for (int i = 0; i < maximumClients  ; i++)
+                            if (clients[i] != null && clients[i].Name != null)
+                                clients[i].ps.println("<" + name + "> " + message);
+                    }
+                }
+            }
+            synchronized (this) 
+            {
+                for (int i = 0; i < maximumClients  ; i++) 
+                    if (clients[i] != null && clients[i] != this && clients[i].Name != null)
+                        clients[i].ps.println("*** " + name + " left the chat room ***");
+            }
+            ps.println("*** See u later " + name + " ***");
+
+            synchronized (this) 
+            {
+                for (int i = 0; i < maximumClients; i++) 
+                    if (clients[i] == this)
+                        clients[i] = null;
+            }
+            dis.close();
+            ps.close();
+            client.close();
+        } 
+        catch (IOException e) {}
+    }
 }
